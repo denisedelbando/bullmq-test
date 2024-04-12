ea2c8b3ca746b9583c426789338fa5acf3072903
Denise Michelle del Bando <denise.delbando@gmail.com>
2024-04-12 10:29:19 -0400

    change to not use client

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,6 @@
-"use client";
 import Image from "next/image";
 
 export default function Home() {
-  const enqueueJob = async () => {
-    const response = await fetch("/api/enqueue", { method: "POST" });
-    const data = await response.json();
-    console.log("Job enqueued with ID:", data.jobId);
-  };
-
   const testEnv = process.env.TEST_ENV ? process.env.TEST_ENV : "doesnt exist";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -17,9 +10,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
         <p>Env : {testEnv}</p>
-        <div>
-          <button onClick={enqueueJob}>Enqueue Job</button>
-        </div>
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
