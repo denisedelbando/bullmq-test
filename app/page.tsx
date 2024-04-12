c816fec7dd057f627decdfae74c48f25ba382126
Denise Michelle del Bando <denise.delbando@gmail.com>
2024-04-12 10:20:52 -0400

    test env

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ export default function Home() {
     const data = await response.json();
     console.log("Job enqueued with ID:", data.jobId);
   };
+
+  const testEnv = process.env.TEST_ENV ? process.env.TEST_ENV : "doesnt exist";
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
@@ -14,6 +16,7 @@ export default function Home() {
           Get started by editing&nbsp;
           <code className="font-mono font-bold">app/page.tsx</code>
         </p>
+        <p>Env : {testEnv}</p>
         <div>
           <button onClick={enqueueJob}>Enqueue Job</button>
         </div>
